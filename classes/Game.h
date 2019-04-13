10fd556cf59b86eac67220b15551e84465a092be
Nonconformizt <bogdankorzh254@mail.ru>
2019-04-13 22:08:06 +0300

    Birch engine basic setup

---
+++ classes/Game.h
@@ -0,0 +1,34 @@
+
+#ifndef PLATFORMER_GAME_H
+#define PLATFORMER_GAME_H
+
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#define WINDOW_W 800
+#define WINDOW_H 600
+#define DELAY 16
+
+class Game {
+public:
+    Game();
+    ~Game();
+
+    void init(const char* title, int width, int height, bool fullscreen);
+
+    void handleEvents();
+    void update();
+    bool running() { return isRunning; }
+    void render();
+    void clean();
+
+private:
+    bool isRunning = false;
+    int cnt = 0;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+};
+
+
+#endif //PLATFORMER_GAME_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
-#include "header.h"
+#include "classes/Game.h"
 
-int main(int argc, char **argv) {
-    cout << "Hello, World!" << endl;
+Game * game = nullptr;
+
+int main(int argc, char *argv[])
+{
+    Uint32 frame_start;
+    int frame_time;
+
+    game = new Game();
+    game->init("Simple platformer", WINDOW_W, WINDOW_H, false);
+
+    while (game->running())
+    {
+        frame_start = SDL_GetTicks();
+
+        game->handleEvents();
+        game->update();
+        game->render();
+
+        frame_time = SDL_GetTicks() - frame_start;
+        if (frame_time < DELAY)
+            SDL_Delay( (Uint32) DELAY - frame_time );
+    }
+
+    game->clean();
     return 0;
 }
