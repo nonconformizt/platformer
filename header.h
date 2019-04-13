ced60cc4fcd0d26a8090031ec9500513bfde41dc
Nonconformizt <bogdankorzh254@mail.ru>
2019-04-13 19:09:29 +0300

    Init

---
+++ header.h
@@ -0,0 +1,14 @@
+
+#ifndef PLATFORMER_HEADER_H
+#define PLATFORMER_HEADER_H
+
+
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+using namespace std;
+
+
+#endif //PLATFORMER_HEADER_H
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "header.h"
+
+int main(int argc, char **argv) {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
